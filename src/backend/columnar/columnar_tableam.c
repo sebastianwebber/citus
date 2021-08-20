da51fd8b5b105c056a729f6d392a7d9b2eb6f13a
Onur Tirtir <onurcantirtir@gmail.com>
2021-08-20 18:18:11 +0300

    Pass ColumnarScanDesc to ColumnarScanChunkGroupsFiltered

--- src/backend/columnar/columnar_tableam.c
+++ src/backend/columnar/columnar_tableam.c
@@ -1762,9 +1762,8 @@ columnar_tableam_finish()
  * Get the number of chunks filtered out during the given scan.
  */
 int64
-ColumnarScanChunkGroupsFiltered(TableScanDesc scanDesc)
+ColumnarScanChunkGroupsFiltered(ColumnarScanDesc columnarScanDesc)
 {
-	ColumnarScanDesc columnarScanDesc = (ColumnarScanDesc) scanDesc;
 	ColumnarReadState *readState = columnarScanDesc->cs_readState;
 
 	/* readState is initialized lazily */
